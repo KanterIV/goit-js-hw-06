6496e6dc7ff5d20beb7d0b24ca98e328ea718ce4
KanterIV <Kanterigor@gmail.com>
2023-07-14 22:56:19 +0200

    task #9 is ready

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const buttonEl = document.querySelector(".change-color");
+const bodyBgColorText = document.querySelector(".color");
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  bodyBgColorText.textContent = document.body.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
