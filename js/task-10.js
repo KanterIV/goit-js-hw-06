a241a2cc4599ac54eb1c7ab2d9ad2beae7bbfed7
KanterIV <Kanterigor@gmail.com>
2023-07-16 18:42:34 +0200

    task 10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -3,34 +3,30 @@ const inputEl = document.querySelector("input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 
-inputEl.addEventListener("input", onInputValue);
-createBtn.addEventListener("click", createBoxes);
+createBtn.addEventListener("click", onCreateClick);
+destroyBtn.addEventListener("click", onDestroyClick);
 
-let userInputValue = 0;
-let markupForAddToBox = "";
-
-function onInputValue() {
-  userInputValue = parseInt(inputEl.value);
+function onCreateClick() {
+  const inputValue = parseInt(inputEl.value);
+  const valueArray = [];
+  let boxWidth = 30;
+  let boxHeigth = 30;
+  for (let i = 0; i < inputValue; i += 1) {
+    const boxesEl = document.createElement("div");
+    boxesEl.style.width = `${boxWidth}px`;
+    boxesEl.style.height = `${boxHeigth}px`;
+    boxesEl.style.backgroundColor = getRandomHexColor();
+    valueArray.push(boxesEl);
+    boxWidth += 10;
+    boxHeigth += 10;
+    boxesList.innerHTML = "";
+  }
+  boxesList.append(...valueArray);
 }
 
-function createBoxes() {
-  for (let i = 0; i < userInputValue; i++) {
-    // const divEl = document.createElement("div");
-    // boxesList.append(divEl);
-    markupForAddToBox = markupForAddToBox + "<div class = item></div>";
-  }
-  // console.log(userInputValue);
-  boxesList.innerHTML += markupForAddToBox;
-  const divArr = document.querySelectorAll(".item");
-  const divArrNew = [...divArr];
-  const divArrFinal = divArrNew.reduce((sizeValue, element) => {
-    sizeValue = sizeValue + 10;
-    element.style.width = `${sizeValue}px`;
-    element.style.heigth = `${sizeValue}px`;
-    console.log(element.style.width);
-    console.log(element.style.heigth);
-  }, 20);
-  console.dir(divArrNew);
+function onDestroyClick() {
+  boxesList.innerHTML = "";
+  inputEl.value = "";
 }
 
 function getRandomHexColor() {
@@ -38,5 +34,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-// const divSize = [];
